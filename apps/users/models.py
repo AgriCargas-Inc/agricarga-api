ca7b5dd21f4f8424e251c4846592c499102130a8
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-06-10 04:49:21 +0100

    feat: add User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,31 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    email = models.EmailField(unique=True, blank=False, null=False)
+    phone = models.CharField(max_length=15, blank=True, null=True, default=None)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    is_active = models.BooleanField("Active", default=True, blank=False, null=False)
+    is_staff = models.BooleanField("Staff", default=False, blank=False, null=False)
+    is_superuser = models.BooleanField(
+        "Superuser", default=False, blank=False, null=False
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Users"
+
+    def get_username(self) -> str:
+        return self.email
+
+    def __str__(self) -> str:
+        return f"{self.email}"
