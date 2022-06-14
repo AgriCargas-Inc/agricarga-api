c3a853f93f3a34059310f070e65c8b36966b0367
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-06-14 23:25:24 +0100

    build: update

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "ninja_jwt",
-    "apps.users"
+    "apps.users",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
