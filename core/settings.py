61f3d8285d710410bcc4ab22f5d18e4bd862f00d
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-06-15 02:37:56 +0100

    chore: collerction file static

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = [os.environ["ORIGIN_HOST"]]
+ALLOWED_HOSTS = ['*']
 
 
 AUTH_USER_MODEL = "users.User"
